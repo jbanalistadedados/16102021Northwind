5917407d35148a03f680557b94a03e172882b439
jbanalistadedados@gmail.com <jbanalistadedados@gmail.com>
2021-10-26 02:05:34 +0000

    21

---
+++ models/northwind/stg_customers.sql
@@ -0,0 +1,16 @@
+with
+    source_data as (
+        select
+            customer_id		
+            , contact_name	
+            , contact_title	
+            , address	
+            , city	
+            , region	
+            , postal_code	
+            , country	
+            , phone	
+            , fax
+            company_name
+        from {{ source ('erpNorthwind16102021','customers')}}
+    )
