4f47d37ea81cd83c877c41db8e9aea5eb1cacbe9
jbanalistadedados@gmail.com <jbanalistadedados@gmail.com>
2021-10-27 01:37:12 +0000

    22

--- models/northwind/stg_products.sql
+++ models/northwind/stg_products.sql
@@ -1,8 +1,18 @@
-{{ config(materialized='table') }}
 with
-    source as 
-        from {{source('erpNorthwind16102021','public_products')}}
+    source_data as (
+        select
+            product_id
+          , product_name
+          , supplier_id
+          , category_id
+          , quantity_per_unit
+          , unit_price
+          , units_in_stock
+          , units_on_order
+          , reorder_level
+          , discontinued                   
+
+        from {{ source('erp_northwind_10_2021', 'public_products') }}          
     )
 
-select *
-from source_data
+select * from source_data
