2a29742f429ceb7a0d38afd9b05191d1ce54a526
jbanalistadedados@gmail.com <jbanalistadedados@gmail.com>
2021-10-28 01:24:56 +0000

    26

---
+++ models/northwind/mart/fact_orders.sql
@@ -0,0 +1,30 @@
+{{ config(materialized='table') }}
+with
+    customer as (
+        select
+            customer_sk
+            customers_id
+        from {{ref('dim_customers')}}
+    )
+
+,  orders_with_sk as (
+        select
+            orders.order_id
+          , customers.customer_sk as customer_fk
+          , employees.employee_sk as employee_fk        
+          , orders.order_date                  
+          , orders.ship_region
+          , orders.shipped_date
+          , orders.ship_country
+          , orders.ship_address
+          , orders.ship_postal_code
+          , orders.ship_city
+          , shippers.shipper_id as shipper_fk
+          , orders.ship_name
+          , orders.freight
+          , orders.required_date
+
+        from {{ ref('stg_orders') }} orders
+        left join customers on orders.customer_id = custumers.customers_id
+        
+select * from orders_with_sk
